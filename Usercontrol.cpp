7f9cde600f17a09a33c1be686d07276480413dfe
TJUisTianjinUniversity <1431789886@qq.com>
2024-07-04 17:36:44 +0800

    Add files via upload

--- Usercontrol.cpp
+++ Usercontrol.cpp
@@ -5,61 +5,91 @@ using namespace vex;
 //底盘移动，四个轮子对应电机
 void VRUN(double l, double r)
 {
-    vexMotorVoltageSet(PORT1, l * 120); //PORT为电机对应的端口
-    vexMotorVoltageSet(PORT3, r * 120);
-    vexMotorVoltageSet(PORT2, l * 120);    
-    vexMotorVoltageSet(PORT4, r * 120);
+    vexMotorVoltageSet(PORT19, l * 120); //PORT为电机对应的端口
+    vexMotorVoltageSet(PORT11, r * 120);
+    vexMotorVoltageSet(PORT20, l * 120);    
+    vexMotorVoltageSet(PORT12, r * 120);
 }
 void Usercontrol()
 {
-  while (true)
-{
-    /***以下为手动部分***/
-    int fb, lf;
+  double ini_angle_control2=Motorarm.position(deg);//arm
+  double ini_angle_control=Motorhand.position(deg);//hand
+
+  while(1)
+    {
+    /***操纵摇杆控制底盘移动***/
+    int fb,lf;
     /********************************************
     相应Axis对应(两个十字对应手柄左右两边摇杆)：
             Axis3
             =
     Axis4 =====
-            =       Axis2
+            =       Axis1
                     =
-            Axis1 =====
+            Axis2 =====
                     =
     *********************************************/
-    fb = Controller1.Axis3.value();
-    lf = Controller1.Axis1.value();
-    fb = abs(fb) > 15 ? fb : 0;
-    lf = abs(lf) > 15 ? lf : 0;
-    if (fb != 0 || lf != 0) VRUN((fb + lf) * 100.0 / 127.0, (fb - lf) * 100.0 / 127.0);
-    else VRUN(0, 0);
+    fb=Controller1.Axis3.value();
+    lf=Controller1.Axis1.value();
+    fb=abs(fb)>15?fb:0;
+    lf=abs(lf)>15?lf:0;
+    if(fb!=0||lf!=0) VRUN((fb+lf)*100.0/127.0,(fb-lf)*100.0/127.0);
+    else VRUN(0,0);
+
 
     //按钮
     //arm
-    if (Controller1.ButtonR1.pressing())
-        Motorarm.spin(forward, 50, pct);//up
-    else if (Controller1.ButtonR2.pressing())
-        Motorarm.spin(reverse, 50, pct);//down
-    else
-        Motorarm.stop(hold);
+    
+    if(Controller1.ButtonR2.pressing()||Controller1.ButtonR1.pressing())
+    {
+      int r2=0,r1=0;
+      if(Controller1.ButtonR2.pressing())
+      r2=1;
+      else if(Controller1.ButtonR1.pressing())
+      r1=1;
+
+      double pre_angle_control2=Motorarm.position(deg);
+
+      if(pre_angle_control2-ini_angle_control2>0.5){
+        for(;pre_angle_control2-ini_angle_control2<110.0;pre_angle_control2=Motorarm.position(deg))
+        {
+
+          if(r2==1)
+            Motorarm.spin(forward,50,pct);//up
+          else if(r1==1)
+            Motorarm.spin(reverse,50,pct);//down
+          
+        }   
+      }   
+      Motorarm.stop(hold);  
+    }
 
     //hand
-    double ini_angle_control=Motorhand.position(deg);
+    
     if(Controller1.ButtonL1.pressing()||Controller1.ButtonL2.pressing())
     {
+      int l2=0,l1=0;
+      if(Controller1.ButtonL2.pressing())
+      l2=1;
+      else if(Controller1.ButtonL1.pressing())
+      l1=1;
+
       double pre_angle_control=Motorhand.position(deg);
-      if(pre_angle_control-ini_angle_control<90.0)
-      {
-        if (Controller1.ButtonL1.pressing())
-            Motorhand.spin(forward, 50, pct);//open
-        else if (Controller1.ButtonL2.pressing())
-            Motorhand.spin(reverse, 50, pct);//close
-        else
-            Motorhand.stop(hold);
+      if(pre_angle_control-ini_angle_control>0.5){
+        for(;pre_angle_control-ini_angle_control<75.0;pre_angle_control=Motorhand.position(deg))
+        {
+
+          if (l1==1)
+              Motorhand.spin(forward, 50, pct);//open
+          else if (l2==1)
+              Motorhand.spin(reverse, 50, pct);//close 
+        } 
       }
-      else
-        Motorhand.stop(hold);  
+      Motorhand.stop(hold);  
     }
-    if(Controller1.ButtonUp.pressing()){
+  
+  //微调位置
+  if(Controller1.ButtonUp.pressing()){
     LeftMotorGroup.spin(forward,10,pct);
     RightMotorGroup.spin(forward,10,pct);
   }
@@ -75,6 +105,7 @@ void Usercontrol()
      LeftMotorGroup.spin(forward,10,pct);
     RightMotorGroup.spin(forward,10,pct);
   }
+
     task::sleep(8);//注意要sleep一小段时间防止过载
-  }    
+    }
 }
