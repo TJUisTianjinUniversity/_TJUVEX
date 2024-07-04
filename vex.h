278e4f923798e7d97b94e1d18925ea91e927743b
TJUisTianjinUniversity <1431789886@qq.com>
2024-07-04 10:25:42 +0800

    Add files via upload

--- vex.h
+++ vex.h
@@ -24,6 +24,56 @@
 
 #define repeat(iterations)                                                     \
   for (int iterator = 0; iterator < iterations; iterator++)
+ struct DistanceController{
+
+	/* 控制因子 */
+	double Kp=2;
+	double Ki=0;
+	double Kd=0;
+
+	/* 设置极限 */
+	double limMin;
+	double limMax;
+
+	/* 积分上限 */
+	double limMinInt;
+	double limMaxInt;
+
+	/* 各项变量 */
+	double integrator;//积分
+	double prevError;//比例
+	double differentiator;//微分
+	double prevMeasurement;//当前状态
+
+  double out;
 
 
+};
+const int UP=1;
+const int DOWN=0;
+const int OPEN=1;
+const int CLOSE=0;
+const double dt=0.001;
+void control_arms(double tar_angle,double pre_angle,int direction,DistanceController* pid);//控制机械臂
+struct coordinate
+{
+    /* data */
+    double x;
+    double y;
+    double toward;
+};
 
+class chassis{
+
+private:
+  coordinate co;
+  void run(double l, double r);
+  void turn(double angle);
+  void move(double distance);
+  void record();
+  
+public:
+  chassis(double x, double y, double toward);
+  void go_to(double x, double y) ;
+  void position(double &x, double &y, double &toward);
+};
