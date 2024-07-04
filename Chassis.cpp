70715c0ccf142c6f3e3608d50488f704a49a4f0f
JieniTurtle <166311148+JieniTurtle@users.noreply.github.com>
2024-07-04 19:39:17 +0800

    Add files via upload

--- Chassis.cpp
+++ Chassis.cpp
@@ -10,7 +10,6 @@ const double T = 0.001;     //采样间隔
 const double tread = 12.13; //底盘轮距
 
 
-
 /*************************************************************
 *成员函数：run
 *函数功能：控制两侧轮子转速
@@ -35,30 +34,37 @@ void chassis::turn(double angle)
 {
     const double T = 0.001;     //采样间隔
 
-    double kp = 2, ki = 0.1, kd = 0.1;   //PID的三个常量，明天一定改好
+    double kp = 10, ki = 0.1, kd = 0.1;   //PID的三个常量，明天一定改好
     double chassis_angle = 0;     //初始位置定为0
-    double error[3] = {angle, angle, angle};   //误差值，errer[0]为当前时刻
-    double motor_pos = LeftMotorGroup.position(rev);   //起始时的电机位置
+    double error_angle = angle, just_error_angle;   //误差值，和前一个误差值
+    double init_pos = LeftMotorGroup.position(rev);   //起始时的电机位置
     double v = 0;    //控制的速度
 
-    while (fabs(error[0]) > 10e-2 && fabs(v) > 10e-2){
-        //更新误差值
-        error[2] = error[1];
-        error[1] = error[0];
-        error[0] = angle - chassis_angle;
+    double integrate_angle = 0;
+    double diff_angle = 0;
 
-        v += (kp + ki + kd) * error[0] - (kp + 2 * kd) * error[1] + kd * error[2];
+    while (fabs(error_angle) > 10e-2 || fabs(v) > 10e-2){
+        Brain.Screen.printAt(10, 40, "error_angle = %f", error_angle);
+        Brain.Screen.printAt(10, 80, "chassis_angle = %f", chassis_angle);
+        just_error_angle = error_angle;     //更新前一个误差值
+        error_angle = angle - chassis_angle;    //更新误差值
+        integrate_angle += error_angle * T;   //计算积分
+        diff_angle = (error_angle - just_error_angle) / T;    //计算微分
+
+        v = kp * error_angle + ki * integrate_angle + kd * diff_angle;  //PID算法计算速度
+        
+        //防止超速
         if (v > max_v){
             run(-max_v, max_v);
         }
         else{
-            run(-v, v);
+            run(-v , v);
         }
 
         //更新位置
-        chassis_angle = propotion * (LeftMotorGroup.position(rev) - motor_pos) / tread * 2;
+        chassis_angle = -propotion * (LeftMotorGroup.position(rev) - init_pos) / tread * 2;
 
-        //间隔 ？？？？？
+        //间隔
         task::sleep(T * 1000);
     }
 }
@@ -71,30 +77,38 @@ void chassis::turn(double angle)
 *********************************************************************/
 void chassis::move(double distance)
 {
-    double kp = 3, ki = 0.1, kd = 0.1;   //PID的三个常量，明天一定改好
-    double pos = 0;     //初始位置定为0
-    double error[3] = {distance, distance, distance};   //误差值，errer[0]为当前时刻
-    double motor_pos = LeftMotorGroup.position(rev);   //起始时的电机位置
+
+    double kp = 2, ki = 0.1, kd = 0.1;   //PID的三个常量，明天一定改好
+    double chassis_route = 0;     //初始位置定为0
+    double error_route = distance, just_error_route;   //误差值，和前一个误差值
+    double init_pos = LeftMotorGroup.position(rev);   //起始时的电机位置
     double v = 0;    //控制的速度
 
-    while (fabs(error[0]) > 10e-2 && fabs(v) > 10e-2){
-        //更新误差值
-        error[2] = error[1];
-        error[1] = error[0];
-        error[0] = distance - pos;
+    double integrate_route = 0;
+    double diff_route = 0;
 
-        v += (kp + ki + kd) * error[0] - (kp + 2 * kd) * error[1] + kd * error[2];
+    while (fabs(error_route) > 10e-2 || fabs(v) > 10e-2){
+        Brain.Screen.printAt(10, 40, "error_angle = %f", error_route);
+        Brain.Screen.printAt(10, 80, "chassis_angle = %f", chassis_route);
+        just_error_route = error_route;     //更新前一个误差值
+        error_route = distance - chassis_route;    //更新误差值
+        integrate_route += error_route * T;   //计算积分
+        diff_route = (error_route - just_error_route) / T;    //计算微分
+
+        v = kp * error_route + ki * integrate_route + kd * diff_route;  //PID算法计算速度
+        
+        //防止超速
         if (v > max_v){
             run(max_v, max_v);
         }
         else{
-            run(v, v);
+            run(v , v);
         }
 
         //更新位置
-        pos = propotion * (LeftMotorGroup.position(rev) - motor_pos);
+        chassis_route = propotion * (LeftMotorGroup.position(rev) - init_pos) / tread * 2;
 
-        //间隔 ？？？？？
+        //间隔
         task::sleep(T * 1000);
     }
 }
@@ -103,30 +117,35 @@ void chassis::move(double distance)
 *函数名称：record
 *函数作用：记录此时的坐标和朝向
 *函数参量：无
-*注意事项：仅在手动阶段调用，别和自动一起用，谢谢
+*注意事项：构造函数时就开始，开一个新线程，到自动结束停止
 *********************************************************************/
-void chassis::record()
-{
-    double left_motor[2] = {LeftMotorGroup.position(rev), LeftMotorGroup.position(rev)};
-    double right_motor[2] = {RightMotorGroup.position(rev), RightMotorGroup.position(rev)};
-    double v_left, v_right;
+// static int record(coordinate co, double toward)
+// {
+//     double left_motor[2] = {LeftMotorGroup.position(rev), LeftMotorGroup.position(rev)};
+//     double right_motor[2] = {RightMotorGroup.position(rev), RightMotorGroup.position(rev)};
+//     double v_left, v_right;
 
-    while(1){
-        left_motor[1] = left_motor[0];
-        left_motor[0] = LeftMotorGroup.position(rev);
-        v_left = propotion * (left_motor[0] - left_motor[1]);
+//     while(1){
+//         left_motor[1] = left_motor[0];
+//         left_motor[0] = LeftMotorGroup.position(rev);
+//         v_left = propotion * (left_motor[0] - left_motor[1]);   //左侧线速度
 
-        right_motor[1] = right_motor[0];
-        right_motor[0] = RightMotorGroup.position(rev);
-        v_right = propotion * (right_motor[0] - right_motor[1]);
+//         right_motor[1] = right_motor[0];
+//         right_motor[0] = RightMotorGroup.position(rev);
+//         v_right = propotion * (right_motor[0] - right_motor[1]);  //右侧线速度
 
-        co.x += sin(toward) * (v_left + v_right) / 2;
-        co.y += cos(toward) * (v_left + v_right) / 2;
-        toward += (-v_left + v_right) / 2 / tread;
+//         //计算速度和转动速度
+//         co.x += sin(toward) * (v_left + v_right) / 2;
+//         co.y += cos(toward) * (v_left + v_right) / 2;
+//         toward += (-v_left + v_right) / 2 / tread / 2 / M_PI;
 
-        task::sleep(T * 1000);
-    }
-}
+//         Brain.Screen.printAt(10, 20, "x = %f", co.x);
+//         Brain.Screen.printAt(10, 50, "y = %f", co.y);
+
+//         task::sleep(T * 1000);
+//     }
+//     return 0;
+// }
 
 /*******************************************************************
 *函数名称：chassis，是本对象的构造函数
@@ -138,6 +157,7 @@ chassis::chassis(double x, double y, double now_toward)
     co.x = x;
     co.y = y;
     toward = now_toward;
+    // task t1(record);
 }
 
 /*******************************************************************
@@ -147,6 +167,7 @@ chassis::chassis(double x, double y, double now_toward)
 *********************************************************************/
 void chassis::go_to(double x, double y, int R) 
 {
+    
     if (R){
         toward += M_PI;
     }
@@ -169,7 +190,6 @@ void chassis::go_to(double x, double y, int R)
             target_angle = atan((y - co.y) / (x - co.x)) + M_PI;; 
         }
     }
-
     //计算要转的角度，范围为[-Π,Π]
     double angle = target_angle - toward;   //要转的角度
     if (angle> M_PI) {
@@ -179,10 +199,13 @@ void chassis::go_to(double x, double y, int R)
         angle += 2 * M_PI;
     }
     turn(angle);    //调旋转函数
-
+    toward = target_angle;
+    Brain.Screen.printAt(0, 0, "angle = %f", angle);
     //前进
     double distance = sqrt(pow((x - co.x), 2) + pow((y - co.y), 2));    //前进的距离
-    move(distance);
+    move(distance - distance_cal);
+    co.x = x - distance_cal - cos(toward);
+    co.y = y - distance_cal - sin(toward);
 }
 
 /*******************************************************************
@@ -196,3 +219,4 @@ void chassis::position(double &x, double &y, double &now_toward)
     y = co.y;
     now_toward = toward;
 }
+
